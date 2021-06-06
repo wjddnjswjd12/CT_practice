cbf6a1be9ef0a74329ac6e2d0734330f987ca889
Jungwon Lee <leejungwon96@gmail.com>
2021-06-06 18:50:18 +0900

    백준 2504.js

--- Baekjoon/6월/2504.js
+++ Baekjoon/6월/2504.js
@@ -1,61 +1,78 @@
-// const readline = require("readline");
+const readline = require("readline");
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// let input = [];
-
-// rl.on("line", function (line) {
-//   input.push(line);
-// }).on("close", function () {
-//   process.exit();
-// });
-
-const test = "(()[[]])([])";
+let input = [];
 let stack = [];
-let answerStr = "";
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  console.log(answer(input[0]));
+  process.exit();
+});
 
 function answer(arr) {
   const temp = arr.split("");
   while (temp.length !== 0) {
     let nextStr = temp.shift();
+    if (nextStr === "{" || nextStr === "}") return 0;
     if (stack.length === 0) stack.push(nextStr);
     else {
+      const secLast = stack[stack.length - 2];
+      const last = stack[stack.length - 1];
+      if (typeof last === "number" && typeof secLast === "number") {
+        const last = stack.pop();
+        const secLast = stack.pop();
+        stack.push(last + secLast);
+      }
       const lastStackStr = stack[stack.length - 1];
       if (lastStackStr === "(") {
-        answerStr += 2;
         if (nextStr === ")") {
-          answerStr += "+";
           stack.pop();
+          stack.push(2);
         } else {
           if (nextStr === "(") {
-            answerStr += "*";
             stack.push(nextStr);
           } else if (nextStr === "[") {
-            answerStr += "*";
             stack.push(nextStr);
           }
         }
       } else if (lastStackStr === "[") {
-        answerStr += 3;
         if (nextStr === "]") {
-          answerStr += "+";
           stack.pop();
+          stack.push(3);
         } else {
           if (nextStr === "[") {
-            answerStr += "*";
             stack.push(nextStr);
           } else if (nextStr === "(") {
             stack.push(nextStr);
-            answerStr += "*";
           }
+        }
+      } else {
+        //lastStackStr 이 숫자일때
+        if (
+          (stack[stack.length - 2] === "(" && nextStr === ")") ||
+          (stack[stack.length - 2] === "[" && nextStr === "]")
+        ) {
+          //
+          stack.pop();
+          stack.pop();
+          const num = nextStr === ")" ? 2 : 3;
+          stack.push(num * lastStackStr);
+        } else {
+          stack.push(nextStr);
         }
       }
     }
   }
-  console.log(stack);
-  console.log(answerStr);
+
+  const answer = stack.reduce((acc, curr) => {
+    acc += curr;
+    return acc;
+  }, 0);
+
+  return typeof answer === "number" && stack.length >= 1 ? answer : 0;
 }
-answer(test);
