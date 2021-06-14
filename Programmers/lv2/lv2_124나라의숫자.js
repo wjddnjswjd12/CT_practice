fc3bac5677ef96f6f26746747f604dba4ab6e37f
Jungwon Lee <leejungwon96@gmail.com>
2021-06-15 07:54:39 +0900

    프로그래머스 124나라의 숫자

--- Programmers/lv2/lv2_124나라의숫자.js
+++ Programmers/lv2/lv2_124나라의숫자.js
@@ -1,3 +1,20 @@
 function solution(n) {
-  return n ? solution((n - (n % 3 || 3)) / 3) + (n % 3 || 4) : "";
+  let answer = "";
+  let temp = n;
+  let leftover = 0;
+
+  while (temp !== 0) {
+    leftover = temp % 3;
+    temp = parseInt(temp / 3);
+
+    if (leftover === 0) {
+      answer = "4" + answer;
+      temp--;
+    } else if (leftover === 1) {
+      answer = "1" + answer;
+    } else if (leftover === 2) {
+      answer = "2" + answer;
+    }
+  }
+  return answer;
 }
