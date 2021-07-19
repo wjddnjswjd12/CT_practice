8a00ce906196b7a85f3924a2b0a0c88590aa2c7d
Jungwon Lee <leejungwon96@gmail.com>
2021-07-20 07:30:48 +0900

    프로그래머스 카펫

--- Programmers/lv2/lv2_카펫.js
+++ Programmers/lv2/lv2_카펫.js
@@ -1,7 +1,21 @@
-function solution(brown, red) {
-  var n = 0;
-  for (var m = red; m >= 1; m--) {
-    n = Math.floor(red / m);
-    if (red % m == 0 && (m + 2) * (n + 2) == brown + red) return [m + 2, n + 2];
+function solution(brown, yellow) {
+  var answer = [];
+  let sum = brown + yellow;
+  const s = Math.sqrt(sum);
+  console.log(s);
+  if (s === parseInt(s)) return [s, s];
+  else {
+    for (let i = parseInt(s); i > 0; i--) {
+      if (sum % i === 0) {
+        let a = i;
+        let b = sum / i;
+        if ((a - 2) * (b - 2) !== yellow) continue;
+        return a > b ? [a, b] : [b, a];
+      }
+    }
   }
+  return answer;
 }
+
+//전에는 답을 참고했는데 이번엔 답 참고 안함 ㅎ
+//그래서 더 길어졌나 ㅋㅋ..ㅠ
