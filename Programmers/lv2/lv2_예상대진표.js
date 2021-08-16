247d0846c57bb65deffc2659d813761938781e7a
Jungwon Lee <leejungwon96@gmail.com>
2021-08-16 21:42:00 +0900

    프로그래머스 예상대진표 lv2

--- Programmers/lv2/lv2_예상대진표.js
+++ Programmers/lv2/lv2_예상대진표.js
@@ -1,4 +1,4 @@
-function solution(n, a, b) {
+function solution(a, b) {
   let answer = 1;
 
   while (Math.ceil(a / 2) !== Math.ceil(b / 2)) {
@@ -9,3 +9,9 @@ function solution(n, a, b) {
 
   return answer;
 }
+
+/*
+처음엔 if문으로 이상한 삽질을 하다가 생각을 바꿨다.
+1,2 3,4가 서로 붙으니  2로 나누어떨어지는 값으 n-1번째 값과 붙는다고 보면 됨
+같은번호가 되면 while문 종료
+*/
